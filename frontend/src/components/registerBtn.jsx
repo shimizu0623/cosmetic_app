79546f8c178b1dd309bcd582987fa9618e70e736
shimizu <github89590207+shimizu0623@users.noreply.github.com>
2022-03-02 13:14:29 +0900

    registerBtnのスタイル変更

--- frontend/src/components/registerBtn.jsx
+++ frontend/src/components/registerBtn.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from "react-router-dom";
 
-
 export const RegisterBtn = () => {
 
     return(
         <>
-        <Button variant="contained" component={RouterLink} to='/'>
+        <Button 
+          variant="contained"
+          sx={{
+            letterSpacing: 3,
+            fontSize: 16,
+            borderRadius: 2,
+            margin: '20px auto 10px',
+            padding: '10px 40px',
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+         }}
+          component={RouterLink} 
+          to='/'>
             登録する
         </Button>
         </>
