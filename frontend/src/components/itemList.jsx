8bcaaabafab6e9dc5846a1360532cb68f3cbec83
shimizu <github89590207+shimizu0623@users.noreply.github.com>
2022-03-13 16:39:54 +0900

    itemList作成

---
+++ frontend/src/components/itemList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ItemList = () => {
+    return(
+        <>
+        <div className='MainContainer'>
+
+         <p>itemList and search</p>
+
+        </div>
+        </>
+    )
+}
