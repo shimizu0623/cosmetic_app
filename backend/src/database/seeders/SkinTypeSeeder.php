f39b52586236371bd80254411c034051463f3fba
shimizu <yoonairi0623@gmail.com>
2022-03-19 15:20:54 +0900

    skintypeの説明文をseederに追加

--- backend/src/database/seeders/SkinTypeSeeder.php
+++ backend/src/database/seeders/SkinTypeSeeder.php
@@ -16,21 +16,27 @@ class SkinTypeSeeder extends Seeder
     {
         DB::table('skin_types')->insert([
             'name' => 'NORMAL SKIN',
+            'detail' => '普通肌：水分と皮脂のバランスが良い状態の肌',
         ]);
         DB::table('skin_types')->insert([
             'name' => 'INNER DRY SKIN',
+            'detail' => 'インナードライ：DRY SKINよりも乾燥が進み、過剰に皮脂が分泌されている状態の肌',
         ]);
         DB::table('skin_types')->insert([
             'name' => 'DRY SKIN',
+            'detail' => '乾燥肌：水分も皮脂も少ない状態の肌',
         ]);
         DB::table('skin_types')->insert([
             'name' => 'OILY SKIN',
+            'detail' => '脂性肌：水分・皮脂ともに多い状態の肌',
         ]);
         DB::table('skin_types')->insert([
             'name' => 'COMBINATION SKIN',
+            'detail' => '混合肌：Tゾーンなど部分的に皮脂が多く、他の部分は乾燥しがちな肌',
         ]);
         DB::table('skin_types')->insert([
             'name' => 'SENSITIVE SKIN',
+            'detail' => '敏感肌：敏感な状態の肌',
         ]);
     }
 }
