f2715a13a70a67ba5c3a4cf0ef3027b8829d1c24
shimizu <yoonairi0623@gmail.com>
2022-03-21 22:26:16 +0900

    ItemListへcheckbox追加

--- frontend/src/pages/itemList.jsx
+++ frontend/src/pages/itemList.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export const ItemList = () => {
+
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = (event) => {
+    setChecked(event.target.checked);
+    };
+
     return(
         <>
         <div className='MainContainer'>
 
          <p>itemList and search</p>
+         <p>test</p>
+
+         <div>
+      <Checkbox
+        checked={checked}
+        onChange={handleChange}
+        color="primary"
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      {/* <Checkbox
+        defaultChecked
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      /> */}
+      {/* <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+      <Checkbox disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
+      <Checkbox disabled checked inputProps={{ 'aria-label': 'disabled checked checkbox' }} />
+      <Checkbox
+        defaultChecked
+        indeterminate
+        inputProps={{ 'aria-label': 'indeterminate checkbox' }}
+      />
+      <Checkbox
+        defaultChecked
+        color="default"
+        inputProps={{ 'aria-label': 'checkbox with default color' }}
+      />
+      <Checkbox
+        defaultChecked
+        size="small"
+        inputProps={{ 'aria-label': 'checkbox with small size' }}
+      /> */}
+    </div>
 
         </div>
         </>
