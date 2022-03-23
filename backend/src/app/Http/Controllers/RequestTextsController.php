ecdb23e3ebf89cac2090f455424a584bf0d153c4
shimizu <yoonairi0623@gmail.com>
2022-03-23 17:11:58 +0900

    RequestをRequestTextへrename

--- backend/src/app/Http/Controllers/RequestTextsController.php
+++ backend/src/app/Http/Controllers/RequestTextsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\RequestText;
+use App\Models\Request as RequestText;
 
 class RequestTextsController extends Controller
 {
--- backend/src/app/Models/RequestText.php
+++ backend/src/app/Models/RequestText.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RequestText extends Model
+class Request extends Model
 {
     use HasFactory;
-
-    protected $table = "requests";
 }
