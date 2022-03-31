9071d57b9cea9387ca8c0a5e37d5f3b9a3b4a547
shimizu <yoonairi0623@gmail.com>
2022-03-31 19:50:42 +0900

    各level_imgを差し替え

--- frontend/src/components/aboutEWG.jsx
+++ frontend/src/components/aboutEWG.jsx
@@ -6,6 +6,16 @@ import Level from "../img/level.PNG"
 import LowHazard from "../img/level_LowHazard.PNG"
 import ModerateHazard from "../img/level_ModerateHazard.PNG"
 import HighHazard from "../img/level_HighHazard.PNG"
+import Level_1 from "../img/level_1.png"
+import Level_2 from "../img/level_2.png"
+import Level_3 from "../img/level_3.png"
+import Level_4 from "../img/level_4.png"
+import Level_5 from "../img/level_5.png"
+import Level_6 from "../img/level_6.png"
+import Level_7 from "../img/level_7.png"
+import Level_8 from "../img/level_8.png"
+import Level_9 from "../img/level_9.png"
+import Level_10 from "../img/level_10.png"
 
 import leaf_green from '../img/leaf_green.png';
 import leaf_yellow from '../img/leaf_yellow.png';
@@ -30,7 +40,20 @@ export const Ewg = (props) => {
               <p><span>「人々の健康を守り、より健康的な環境で生活をおくるための後押しをする」</span>というミッションのもと</p>
               <p>食品やコスメなどの分野において活動を続けている、アメリカの非営利環境市民団体です。</p>
               <p>コスメに関してはEWGの中でSkin Deepと呼ばれる分野において、安全を確認することができます。</p>
-              <img src={Level} id="EwgLevel_img"  alt="EwgLevel_img" />
+              {/* <img src={Level} id="EwgLevel_img"  alt="EwgLevel_img" /> */}
+
+              <img src={Level_1} alt="Level_1_img" id="EwgLevel_img" />
+              <img src={Level_2} alt="Level_2_img" id="EwgLevel_img" />
+              <img src={Level_3} alt="Level_3_img" id="EwgLevel_img" />
+              <img src={Level_4} alt="Level_4_img" id="EwgLevel_img" />
+              <img src={Level_5} alt="Level_5_img" id="EwgLevel_img" />
+              <img src={Level_6} alt="Level_6_img" id="EwgLevel_img" />
+              <img src={Level_7} alt="Level_7_img" id="EwgLevel_img" />
+              <img src={Level_8} alt="Level_8_img" id="EwgLevel_img" />
+              <img src={Level_9} alt="Level_9_img" id="EwgLevel_img" />
+              <img src={Level_10} alt="Level_10_img" id="EwgLevel_img" />
+
+
               <p>化粧品原料の有害性の程度を<span>1~10のレベル</span>で示してくれているので、</p>
               <p>コスメの安全性を簡単に判断することができるのです。</p>
 
@@ -41,15 +64,25 @@ export const Ewg = (props) => {
               <p>EWG等級は、数字が低ければ低いほど有害性の低い安全な成分となっています。</p>
               <p>つまり、<span>1が最も安全で、10が最も危険</span>ということをを示しています。</p>
                   <div className='level_LowHazard'>
-                    <img src={LowHazard} id="LowHazard_img"  alt="LowHazard_img" />
+                    {/* <img src={LowHazard} id="LowHazard_img"  alt="LowHazard_img" /> */}
+                    <img src={Level_1} alt="Level_1_img" id="Hazard_img" />
+                    <img src={Level_2} alt="Level_2_img" id="Hazard_img" />
                     <p>1~2等級 → 有害性が低い成分（緑色）</p>
                   </div>
                   <div className='level_ModerateHazard'>
-                    <img src={ModerateHazard} id="ModerateHazard_img"  alt="ModerateHazard_img" />
+                    {/* <img src={ModerateHazard} id="ModerateHazard_img"  alt="ModerateHazard_img" /> */}
+                    <img src={Level_3} alt="Level_3_img" id="Hazard_img" />
+                    <img src={Level_4} alt="Level_4_img" id="Hazard_img" />
+                    <img src={Level_5} alt="Level_5_img" id="Hazard_img" />
+                    <img src={Level_6} alt="Level_6_img" id="Hazard_img" />
                     <p>3~6等級 → 有害性が普通の成分（黄色）</p>
                   </div>
                   <div className='level_HighHazard'>
-                    <img src={HighHazard} id="HighHazard_img"  alt="HighHazard_img" />
+                    {/* <img src={HighHazard} id="HighHazard_img"  alt="HighHazard_img" /> */}
+                    <img src={Level_7} alt="Level_7_img" id="Hazard_img" />
+                    <img src={Level_8} alt="Level_8_img" id="Hazard_img" />
+                    <img src={Level_9} alt="Level_9_img" id="Hazard_img" />
+                    <img src={Level_10} alt="Level_10_img" id="Hazard_img" />
                     <p>7~10等級 → 有害性が高い成分（赤色）</p>
                   </div>
               <p>「EWG Green等級」は、1~2等級の安全性が高い等級のことです。</p>
