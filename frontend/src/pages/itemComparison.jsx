9461a445e284e6c7deebcb35c2f0716a2784a5d5
shimizu <yoonairi0623@gmail.com>
2022-04-03 22:22:09 +0900

    商品比較ページフォーム作成

--- frontend/src/pages/itemComparison.jsx
+++ frontend/src/pages/itemComparison.jsx
@@ -1,12 +1,114 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from '../axios';
+import header_img from '../img/headerComparison.jpg';
+import leaf_green from '../img/leaf_green.png';
+import leaf_yellow from '../img/leaf_yellow.png';
+import leaf_brown from '../img/leaf_brown.png';
+
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import { makeStyles } from "@material-ui/core/styles";
+import Link from '@mui/material/Link';
+import { Link as RouterLink } from "react-router-dom";
+
+
+const useStyles = makeStyles({
+    tableHeader: {
+        background: '#e4e4e4',
+        minWidth: '100px',
+    },
+})
 
 export const ItemComparison = () => {
+    const classes = useStyles();
+    const [item, setItem] = useState(null);
+    // const [ingredients, setIngredients] = useState(null);
+    
+    useEffect(async () => {
+        const responseItem = await axios.get('/item')
+        // const responseIngredients = await axios.get('/item_ingredients')
+        const i = responseItem.data
+        // const ingredient = responseIngredients.data
+        setItem(i)
+        // setIngredients(ingredient)
+    }, [])
+
+    const itemInformation = () => {
+        if(item === null){
+            return <CircularProgress color="success" size="15px" />
+        }
+        return(
+                // TODO: ↓item_ingredientsのデータ？
+                <tr>
+                    <th scope="row"><img src={item.img} alt="itemImg" style={{ maxWidth: '90px', height: '100%', margin: 'auto 30px' }} /></th>
+                    <td>{item.brand}</td>
+                    <th>{item.name}</th>
+                    <td>{item.volume}</td>
+                    <td>￥{item.price}</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td><Button 
+                            variant="contained" 
+                            style={{
+                                marginTop: '10px',
+                                color: 'white',
+                                background: 'rgba(141, 203, 193)',
+                                borderRadius: '7px',
+                            }}
+                            onClick={onClickDelete}>
+                        削除
+                        </Button>
+                    </td>
+                </tr>   
+        )
+    }
+
+    const onClickDelete = () => {
+        console.log('onClickDelete')
+    }
+
+
     return(
         <>
         <div className='MainContainer'>
+        <img src={header_img} alt="header" style={{ width: '100%' }}/>
+        <div>
+            <div style={{marginTop: '50px'}}>
+                <p>商品の違いに悩む時はありませんか？</p>
+                <p>そんな時は、このページでアイテム情報を比較してみることができます。</p>
+                <p>また、アイテムの画像部分をクリックすると、詳細を確認することができます。</p>
+                <p><Link component={RouterLink} to="/itemSearch">→比較したい商品を探す</Link></p>
+            </div>
 
-         <p>商品比較</p>
 
+            <div style={ {margin: '50px 0 20px 0' }}>
+                <table style={{ margin: '0 auto' }}>
+                <tr>
+                    <th className={classes.tableHeader}></th>
+                    <th className={classes.tableHeader}>ブランド</th>
+                    <th className={classes.tableHeader}>商品名</th>
+                    <th className={classes.tableHeader}>容量</th>
+                    <th className={classes.tableHeader}>価格</th>
+                    <th className={classes.tableHeader}><img src={leaf_green} alt="ewg_green" style={{ width: '30px' }} />成分数</th>
+                    <th className={classes.tableHeader}><img src={leaf_yellow} alt="ewg_yellow" style={{ width: '30px' }} />成分数</th>
+                    <th className={classes.tableHeader}><img src={leaf_brown} alt="ewg_red" style={{ width: '30px' }} />成分数</th>
+                    <th className={classes.tableHeader}>発がん性</th>
+                    <th className={classes.tableHeader}>発達/生殖毒性</th>
+                    <th className={classes.tableHeader}>免疫毒性</th>
+                </tr>
+
+                {itemInformation()}
+
+
+                </table>
+            </div>
+
+                <p>コスメ比較へ追加されたアイテムはありません。</p>
+        </div>
         </div>
         </>
     )
