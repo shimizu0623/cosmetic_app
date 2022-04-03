de7dc723fd85c6179959fd420fd44de5211c6170
shimizu <yoonairi0623@gmail.com>
2022-04-03 14:23:32 +0900

    thanksPageデザイン作成

--- frontend/src/pages/thanksPage.jsx
+++ frontend/src/pages/thanksPage.jsx
@@ -1,12 +1,21 @@
+
 import React from 'react';
+import header_img from '../img/headerThanks.jpg';
+import { Btn } from '../components/btn';
+import { Link as RouterLink } from "react-router-dom";
 
 export const Thanks = () => {
     return(
         <>
         <div className='MainContainer'>
-
-         <p>thanks</p>
-
+        <img src={header_img} alt="header" style={{ width: '100%' }}/>
+        <div style={{ margin: '40px auto' }}>
+            <h1>リクエスト送信完了</h1>
+            <p>この度は貴重なご意見をお送りいただき、誠にありがとうございます。</p>
+            <p>お寄せいただいた内容を参考に、サービス改善へ努めて参ります。</p>
+            <p>今後ともどうぞよろしくお願いします。</p>
+        </div>
+            <Btn message='マイページへ戻る' component={RouterLink} to="/myPage" />
         </div>
         </>
     )
