fd3374886a9c3b397f9e622776bedbb4203f307a
shimizu <yoonairi0623@gmail.com>
2022-04-05 17:01:22 +0900

    imgの追加とメッセージの変更

--- frontend/src/pages/deleteConfirmPage.jsx
+++ frontend/src/pages/deleteConfirmPage.jsx
@@ -3,11 +3,14 @@ import { Btn } from '../components/btn';
 import { Link as RouterLink } from "react-router-dom";
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 
+import green_leaf from '../img/green_leaf_img.jpg';
+
 export const DeleteConfirm = () => {
     return(
         <>
         <div className='MainContainer'>
-            <h1>退会フォーム</h1>
+            <img src={green_leaf} alt="" style={{ maxWidth: '90px', display: 'inline-block', verticalAlign: 'middle', margin: '0 auto 40px' }} />
+            <h1 style={{ fontSize: '40px', display: 'inline-block' }}>退会フォーム</h1>
             <div>
                 <p>いつもご利用いただき、ありがとうございます。</p>
                 <p>よろしければ、退会の理由をお聞かせください。</p>
@@ -19,8 +22,8 @@ export const DeleteConfirm = () => {
                 />
             </div>
             <div style={{margin: '30px auto'}}>
-                <p>退会すると、登録データが全て削除されます。</p>
-                <p>よろしければ、下のボタンより退会手続きを行ってください。</p>
+                <p>下のボタンより退会手続きを行ってください。</p>
+                <p>退会すると、登録データは全て削除されます。</p>
             </div>
 
             <Btn message='退会する' component={RouterLink} to="/delete"/>
