e42fc7cafb895693d260e312f7a9c8acd6ed7ab6
shimizu <yoonairi0623@gmail.com>
2022-04-16 17:02:34 +0900

    ファイル作成、index追加

---
+++ backend/src/app/Http/Controllers/UserUnmatchedItemsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserUnmatchedItem;
+use App\Models\Item;
+use \Symfony\Component\HttpFoundation\Response;
+
+class UserUnmatchedItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        $unmatchedItems = Item::userUnmatchedOnly($user->id)
+        ->get();
+
+        return response()->json(
+            $unmatchedItems->map(function ($item) {
+                return $item->toArray();
+            })
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
