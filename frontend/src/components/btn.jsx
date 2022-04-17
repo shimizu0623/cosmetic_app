b19dfb9bcdfe7099c05850adda4a4e54a5ff459f
shimizu <yoonairi0623@gmail.com>
2022-04-18 00:43:10 +0900

    styleがきくように修正

--- frontend/src/components/btn.jsx
+++ frontend/src/components/btn.jsx
@@ -1,35 +1,23 @@
 // MEMO: スタイル調整済
 import React from 'react';
 import Button from '@mui/material/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  root: {
-    color: 'white',
-    padding: '10px 25px', 
-    fontSize: '17px',
-    letterSpacing: 2,
-    borderRadius: '5px',
-    background: '#73baae',
-    '&:hover': {
-      background: 'rgba(141, 203, 193)', 
-    },
-}});
-
 
 export const Btn = (props) => {
-  const classes = useStyles();
-  const {message} = props;
+    const {message} = props;
 
-  return(
-      <>
-      <Button
-        variant="contained"
-        className={classes.root}
-        {...props}
-      >
-        {message}
-      </Button>
-      </>
-  );
+    return(
+        <Button
+          variant="contained"
+          style={{
+            color: 'white',
+            padding: '10px 25px', 
+            fontSize: '17px',
+            letterSpacing: 2,
+            background: '#73baae',
+            borderRadius: '5px',
+          }}
+          {...props}
+        >{message}
+        </Button>
+    );
 };
