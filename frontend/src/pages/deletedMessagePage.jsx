003ca6a7819089fd2ed01cb94490100ef27a972a
shimizu <yoonairi0623@gmail.com>
2022-04-18 01:07:44 +0900

    スタイル調整

--- frontend/src/pages/deletedMessagePage.jsx
+++ frontend/src/pages/deletedMessagePage.jsx
@@ -1,16 +1,19 @@
+// MEMO: スタイル調整済
 import React from 'react';
 import { Btn } from '../components/btn';
 import { Link as RouterLink } from "react-router-dom";
+import green_leaf from '../img/green_leaf_img_clear.png';
 
 export const DeleteInformation = () => {
     return(
         <>
         <div className='MainContainer'>
-            <h1>退会完了</h1>
-            <p>ご利用いただき、ありがとうございました。</p>
+            <img src={green_leaf} alt="" style={{ maxWidth: '90px', display: 'inline-block', verticalAlign: 'middle', margin: '0 auto 40px' }} />
+            <h1 style={{ fontSize: '40px', display: 'inline-block' }}>退会完了</h1>
+            <p>ご利用いただき、誠にありがとうございました。</p>
             <p>またのご利用をお待ちしております。</p>
         </div>
-            <Btn message='TOPへ戻る' component={RouterLink} to="/" />
+        <Btn message='TOPへ戻る' component={RouterLink} to="/" />
         </>
     )
 }
