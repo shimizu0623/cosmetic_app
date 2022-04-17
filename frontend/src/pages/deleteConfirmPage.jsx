57e2dbb043396aa1f4ae8a26e486a7e7301f041f
shimizu <yoonairi0623@gmail.com>
2022-04-18 01:09:19 +0900

    APIへ退会理由登録できるように追記

--- frontend/src/pages/deleteConfirmPage.jsx
+++ frontend/src/pages/deleteConfirmPage.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from '../axios';
+import { useNavigate } from "react-router-dom";
 import { GoBackBtn } from '../components/goBackBtn';
 import { Btn } from '../components/btn';
 import { Link as RouterLink } from "react-router-dom";
@@ -6,10 +8,44 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import green_leaf from '../img/green_leaf_img_clear.png';
 
 export const DeleteConfirm = () => {
+    const [reason,setReason] = useState(null);
+    const navigate = useNavigate();
 
-    const handleSend = () => {
-        console.log('handleSend');
-    }
+    const handleAddReason = (event) => {
+        setReason({...reason, reason:event.target.value});
+        console.log(reason);
+    };
+
+    // TODO: 入力日を指定する
+    const handleSend = async () => {
+        if (reason === null){
+            try {
+                console.log('handleSend');
+                const response = await axios.post('/leave_reasons', {
+                    reason: '未記入',
+                    leave_date: '2022-01-01',
+                });
+                navigate("/delete");
+            } catch (e) {
+                window.alert('送信に失敗しました');
+                console.error(e)
+                return;
+            }
+        } else {
+            try {
+                console.log('handleSend');
+                const response = await axios.post('/leave_reasons', {
+                    reason: reason.reason,
+                    leave_date: '2022-01-01',
+                });
+                navigate("/delete");
+            } catch (e) {
+                window.alert('送信に失敗しました');
+                console.error(e)
+                return;
+            }
+        };
+    };
 
     return(
         <>
@@ -25,6 +61,7 @@ export const DeleteConfirm = () => {
                 aria-label="minimum height"
                 minRows={20}
                 style={{ width: 400 }}
+                onChange={handleAddReason}
                 />
             </div>
             <div style={{margin: '30px auto'}}>
@@ -33,7 +70,6 @@ export const DeleteConfirm = () => {
             </div>
 
             <Btn message='退会する' onClick={handleSend}/>
-            {/* <Btn message='退会する' component={RouterLink} to="/delete" onClick={handleSend}/> */}
 
         </div>
         </>
