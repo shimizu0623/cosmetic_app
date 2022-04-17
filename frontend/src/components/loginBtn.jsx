450dcc387983d749a2a0e36cf5d4571be6b88743
shimizu <yoonairi0623@gmail.com>
2022-04-17 13:44:24 +0900

    スタイル調整

--- frontend/src/components/loginBtn.jsx
+++ frontend/src/components/loginBtn.jsx
@@ -1,11 +1,18 @@
+// MEMO: スタイル調整済
 import React from 'react';
 import Button from '@mui/material/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link as RouterLink } from "react-router-dom";
 
-
 const useStyles = makeStyles({
   root: {
+    color: 'white',
+    letterSpacing: 3,
+    fontSize: 16,
+    borderRadius: 5,
+    margin: '10px auto',
+    padding: '10px 40px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     height: 60,
     maxWidth: '300px',
     background: 'linear-gradient(45deg, #00bcd4 30%, #1de9b6 90%)',
@@ -15,27 +22,17 @@ const useStyles = makeStyles({
 }});
 
 export const LoginBtn = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-
-    return(
-        <>
-            <Button
-              variant="contained"
-              className={classes.root}
-              style={{
-                color: 'white',
-                letterSpacing: 3,
-                fontSize: 16,
-                borderRadius: 3,
-                margin: '10px auto',
-                padding: '10px 40px',
-                boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-              }}
-              component={RouterLink}
-              to="/login">
-            無料ではじめる
-            </Button>
-        </>
-    )
+  return(
+    <>
+        <Button
+          variant="contained"
+          className={classes.root}
+          component={RouterLink}
+          to="/login">
+        無料ではじめる
+        </Button>
+    </>
+  )
 }
