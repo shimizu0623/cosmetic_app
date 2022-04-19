4989857145dc838165a55172facec3fcedc07e8a
shimizu <yoonairi0623@gmail.com>
2022-04-19 18:06:07 +0900

    store削除してdeleteMeに移動

--- backend/src/app/Http/Controllers/LeaveReasonsController.php
+++ backend/src/app/Http/Controllers/LeaveReasonsController.php
@@ -23,37 +23,6 @@ class LeaveReasonsController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // TODO: ↓これなくてもエラー出なかったから要らない？
-        // $date = new Carbon();
-        $date = Carbon::now(); 
-
-        $validator = Validator::make($request->all(),[
-            // ↓TODO: 必須ではない、入力なしokにするにはfrontendに書いてるのでOK？
-            'reason' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
-        $create = LeaveReason::create([
-            'reason' => $request->reason,
-            // ↓TODO: 'leave_date' => Carbon::now(),の方が良い？
-            'leave_date' => $date,
-        ]);
-
-        return response()->json($create, Response::HTTP_OK);
-
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
