d6555c3fe26692fbcc8e173964e3b79c1c96a4e0
shimizu <yoonairi0623@gmail.com>
2022-04-20 00:50:50 +0900

    TODO追加

--- frontend/src/pages/itemComparisonPage.jsx
+++ frontend/src/pages/itemComparisonPage.jsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles({
         background: '#e4e4e4',
         minWidth: '100px',
     },
-})
+});
 
 export const ItemComparison = () => {
     const classes = useStyles();
@@ -28,25 +28,25 @@ export const ItemComparison = () => {
     // const [ingredients, setIngredients] = useState(null);
     
     useEffect(async () => {
-        const responseItem = await axios.get('/item')
+        const responseItem = await axios.get('/item');
         // const responseIngredients = await axios.get('/item_ingredients')
-        const i = responseItem.data
+        const i = responseItem.data;
         // const ingredient = responseIngredients.data
-        setItem(i)
+        setItem(i);
         // setIngredients(ingredient)
-    }, [])
+    }, []);
 
 
-    const onClickDetail = () => navigate(`/item/${item.id}`);
+    const handleDetail = () => navigate(`/item/${item.id}`);
 
     const itemInformation = () => {
-        if(item === null){
+        if (item === null){
             return <CircularProgress color="success" size="15px" />
         }
         return(
                 // TODO: ↓item_ingredientsのデータ？
                 <tr>
-                    <td scope="row"><img src={item.img} alt="itemImg" style={{ maxWidth: '90px', height: '100%', margin: 'auto 30px' }} onClick={onClickDetail} /></td>
+                    <td scope="row"><img src={item.img} alt="itemImg" style={{ maxWidth: '90px', height: '100%', margin: 'auto 30px' }} onClick={handleDetail} /></td>
                     <td>{item.brand}</td>
                     <td>{item.name}</td>
                     <td>{item.volume}</td>
@@ -63,72 +63,71 @@ export const ItemComparison = () => {
                                 background: 'rgba(141, 203, 193)',
                                 borderRadius: '7px',
                             }}
-                            onClick={onClickDelete}>
+                            onClick={handleDelete}>
                         削除
                         </Button>
                     </td>
                 </tr>   
         )
-    }
-
-    const onClickDelete = () => {
-        const confirmMessage = '削除してよろしいですか？'
+    };
+    // TODO: ifの時の処理↓
+    const handleDelete = () => {
+        const confirmMessage = '削除してよろしいですか？';
         let result = window.confirm(confirmMessage);
-        if(result){
-            console.log('onClickDelete')       
-        }else{
+        if (result){
+            console.log('handleDelete')   ;    
+        } else {
             return;
         }    
-    }
+    };
 
 
-    const explain_green = 'EWG 1~2等級（有害性が低い成分）'
-    const explain_yellow = 'EWG 3~6等級（有害性が普通の成分）'
-    const explain_red = 'EWG 7~10等級（有害性が高い成分）'
+    const explain_green = 'EWG 1~2等級（有害性が低い成分）';
+    const explain_yellow = 'EWG 3~6等級（有害性が普通の成分）';
+    const explain_red = 'EWG 7~10等級（有害性が高い成分）';
 
 
     return(
         <>
         <div className='MainContainer'>
-        <GoBackBtn />
-        <img src={header_img} alt="header" style={{ width: '100%' }}/>
-        <div>
-            <div style={{marginTop: '50px'}}>
-                <p>商品の違いに悩む時はありませんか？</p>
-                <p>そんな時は、このページでアイテム情報を比較してみることができます。</p>
-                <p>また、アイテムの画像部分をクリックすると、詳細を確認することができます。</p>
-                <p><Link component={RouterLink} to="/itemSearch">→比較したい商品を探す</Link></p>
-            </div>
+            <GoBackBtn />
+            <img src={header_img} alt="header" style={{ width: '100%' }}/>
+            <div>
+                <div style={{ marginTop: '50px' }}>
+                    <p>商品の違いに悩む時はありませんか？</p>
+                    <p>そんな時は、このページでアイテム情報を比較してみることができます。</p>
+                    <p>また、アイテムの画像部分をクリックすると、詳細を確認することができます。</p>
+                    <p><Link component={RouterLink} to="/itemSearch">→比較したい商品を探す</Link></p>
+                </div>
 
 
-            <div style={ {margin: '50px 0 20px 0' }}>
-                <table style={{ margin: '0 auto' }}>
-                <tr>
-                    <th className={classes.tableHeader}></th>
-                    <th className={classes.tableHeader}>ブランド</th>
-                    <th className={classes.tableHeader}>商品名</th>
-                    <th className={classes.tableHeader}>容量</th>
-                    <th className={classes.tableHeader}>価格</th>
-                    <Tooltip title={explain_green} followCursor>
-                    <th className={classes.tableHeader}><img src={leaf_green} alt="ewg_green" style={{ width: '30px' }} />成分数</th>
-                    </Tooltip>
-                    <Tooltip title={explain_yellow} followCursor>
-                    <th className={classes.tableHeader}><img src={leaf_yellow} alt="ewg_yellow" style={{ width: '30px' }} />成分数</th>
-                    </Tooltip>
-                    <Tooltip title={explain_red} followCursor>
-                    <th className={classes.tableHeader}><img src={leaf_brown} alt="ewg_red" style={{ width: '30px' }} />成分数</th>
-                    </Tooltip>
-                    <th className={classes.tableHeader}>評価レビュー</th>
-                </tr>
+                <div style={{ margin: '50px 0 20px 0' }}>
+                    <table style={{ margin: '0 auto' }}>
+                        <tr>
+                            <th className={classes.tableHeader}></th>
+                            <th className={classes.tableHeader}>ブランド</th>
+                            <th className={classes.tableHeader}>商品名</th>
+                            <th className={classes.tableHeader}>容量</th>
+                            <th className={classes.tableHeader}>価格</th>
+                            <Tooltip title={explain_green} followCursor>
+                            <th className={classes.tableHeader}><img src={leaf_green} alt="ewg_green" style={{ width: '30px' }} />成分数</th>
+                            </Tooltip>
+                            <Tooltip title={explain_yellow} followCursor>
+                            <th className={classes.tableHeader}><img src={leaf_yellow} alt="ewg_yellow" style={{ width: '30px' }} />成分数</th>
+                            </Tooltip>
+                            <Tooltip title={explain_red} followCursor>
+                            <th className={classes.tableHeader}><img src={leaf_brown} alt="ewg_red" style={{ width: '30px' }} />成分数</th>
+                            </Tooltip>
+                            <th className={classes.tableHeader}>評価レビュー</th>
+                        </tr>
 
-                {itemInformation()}
+                        {itemInformation()}
 
-
-                </table>
-            </div>
+                    </table>
+                </div>
 
                 <p>コスメ比較へ追加されたアイテムはありません。</p>
-        </div>
+            </div>
         </div>
         </>
     )
