4052830ad165b65910edef94295b8dcf8b5f8b90
shimizu <yoonairi0623@gmail.com>
2022-04-25 14:43:29 +0900

    logoImg追加とホームへ戻るonClickをlogoの方へ変更

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,9 +4,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { Link as RouterLink } from "react-router-dom";
 import ListItemIcon from '@mui/material/ListItemIcon';
-// import icon from '../img/.png';
+import logo_img from "../img/CS_logo.png";
 import menuIcon from '../img/menuIcon.png';
-import HomeIcon from '@material-ui/icons/Home';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 import FolderSpecialIcon from '@material-ui/icons/FolderSpecial';
@@ -21,7 +20,7 @@ import Logout from '@mui/icons-material/Logout';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  header: {
+  headerRight: {
     position: 'fixed',
     textAlign: 'right',
     top: '0',
@@ -29,6 +28,12 @@ const useStyles = makeStyles({
     height: '60px',
     lineHeight: '60px',
     background: 'rgba(243, 240, 240, 0.734)',
+    },
+    headerLeft: {
+    position: 'fixed',
+    textAlign: 'left',
+    top: '0',
+    zIndex: '2147483647'
     },
 });
 
@@ -40,6 +45,7 @@ export const Header = () => {
   const openSearch = Boolean(anchorElSearch);
   const openHelp = Boolean(anchorElHelp);
   const open = Boolean(anchorEl);
+
   const handleSearchClick = (event) => {
       setAnchorElSearch(event.currentTarget);
   };
@@ -55,182 +61,176 @@ export const Header = () => {
       setAnchorEl(null);
   };
 
-
-return(
+  return(
     <>
-    <header className={classes.header}>
-      <div>
-      {/* <img src={icon} alt="logo" style={{ width: '70px' }}/> */}
-      <Button
-        id="basic-button"
-        aria-controls={openSearch ? 'search-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={openSearch ? 'true' : undefined}
-        onClick={handleSearchClick}
-        style={{
-            fontSize: '20px', 
-            fontFamily: 'bold', 
-            padding: '0 15px',
-            color: '#455a64',
-            letterSpacing: '2px',
-            lineHeight: '60px',
-          }} 
-      >
-        <SearchIcon />
-        SEARCH
-      </Button>
-      <Menu
-        id="search-menu"
-        anchorEl={anchorElSearch}
-        open={openSearch}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        style={{
-            marginTop: '45px',
-          }} 
-      >
-
-        <MenuItem component={RouterLink} to="/itemSearch">
-          <ListItemIcon>
+      <header>
+        <div className={classes.headerLeft}>
+          <Button
+            component={RouterLink}
+            to="/homePage"
+            style={{ 
+              padding: '0'
+            }}
+          >
+            <img src={logo_img} alt="logo" style={{ margin: '0 30px', height: '60px' }} />  
+          </Button>
+        </div>
+        <div className={classes.headerRight}>
+          <Button
+            id="basic-button"
+            aria-controls={openSearch ? 'search-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={openSearch ? 'true' : undefined}
+            onClick={handleSearchClick}
+            style={{
+              fontSize: '20px', 
+              fontFamily: 'bold', 
+              padding: '0 15px',
+              color: '#455a64',
+              letterSpacing: '2px',
+              lineHeight: '60px',
+            }} 
+          >
             <SearchIcon />
-          </ListItemIcon>
-          アイテムを探す
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/ranking">
-          <ListItemIcon>
-            <EmojiEventsIcon />
-          </ListItemIcon>
-          ランキング
-        </MenuItem>
-      </Menu>
-      
-      <Button
-        id="basic-button"
-        aria-controls={openHelp ? 'help-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={openHelp ? 'true' : undefined}
-        onClick={handleHelpClick}
-        style={{
-            fontSize: '20px', 
-            fontFamily: 'bold', 
-            padding: '0 15px',
-            color: '#455a64',
-            letterSpacing: '2px',
-            lineHeight: '60px',
-          }} 
-      >
-        <HelpOutlineIcon />
-        HELP
-      </Button>
-      <Menu
-        id="help-menu"
-        anchorEl={anchorElHelp}
-        open={openHelp}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        style={{
-            marginTop: '45px',      
-          }} 
-      >
-        <MenuItem component={RouterLink} to="/ewgPage">
-          <ListItemIcon>
-            <ContactSupportIcon />
-          </ListItemIcon>
-          EWGってなに？
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/helpPage">
-          <ListItemIcon>
+            SEARCH
+          </Button>
+          <Menu
+            id="search-menu"
+            anchorEl={anchorElSearch}
+            open={openSearch}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            style={{
+              marginTop: '45px',
+            }} 
+          >
+            <MenuItem component={RouterLink} to="/itemSearch">
+              <ListItemIcon>
+                <SearchIcon />
+              </ListItemIcon>
+              アイテムを探す
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/ranking">
+              <ListItemIcon>
+                <EmojiEventsIcon />
+              </ListItemIcon>
+              ランキング
+            </MenuItem>
+          </Menu>
+        
+          <Button
+            id="basic-button"
+            aria-controls={openHelp ? 'help-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={openHelp ? 'true' : undefined}
+            onClick={handleHelpClick}
+            style={{
+              fontSize: '20px', 
+              fontFamily: 'bold', 
+              padding: '0 15px',
+              color: '#455a64',
+              letterSpacing: '2px',
+              lineHeight: '60px',
+            }} 
+          >
             <HelpOutlineIcon />
-          </ListItemIcon>
-          使い方ヘルプ
-        </MenuItem>
-      </Menu>
+            HELP
+          </Button>
+          <Menu
+            id="help-menu"
+            anchorEl={anchorElHelp}
+            open={openHelp}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            style={{
+              marginTop: '45px',      
+            }} 
+          >
+            <MenuItem component={RouterLink} to="/ewgPage">
+              <ListItemIcon>
+                <ContactSupportIcon />
+              </ListItemIcon>
+              EWGってなに？
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/helpPage">
+              <ListItemIcon>
+                <HelpOutlineIcon />
+              </ListItemIcon>
+              使い方ヘルプ
+            </MenuItem>
+          </Menu>
 
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleAccountClick}
-        style={{
-            fontSize: '25px', 
-            fontFamily: 'bold', 
-            padding: '0 30px 0 15px',
-            margin: '0 auto',
-            height: '60px',
-          }} 
-      >
-        <img src={menuIcon} alt="logo" style={{ width: '70px' }}/>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        style={{
-            marginTop: '45px',      
-          }} 
-      >
-        {/* TODO: ホームへ戻るはサイトロゴが出来たら移動する */}
-        <MenuItem component={RouterLink} to="/homePage">
-          <ListItemIcon>
-          <HomeIcon />
-          </ListItemIcon>
-          ホームへ戻る
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/myPage">
-          <ListItemIcon>
-            <FavoriteBorderIcon />
-          </ListItemIcon>
-          マイページ
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/myFolder">
-          <ListItemIcon>
-            <FolderSpecialIcon />
-          </ListItemIcon>
-          マイフォルダ
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/itemComparison">
-          <ListItemIcon>
-            <EqualizerIcon />
-          </ListItemIcon>
-          コスメ比較
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/unmatchedItem">
-          <ListItemIcon>
-            <NotInterestedIcon />
-          </ListItemIcon>
-          肌に合わなかったアイテム
-        </MenuItem>
-        <MenuItem component={RouterLink} to="/requestPage">
-          <ListItemIcon>
-            <MailOutlineIcon />
-          </ListItemIcon>
-          リクエスト
-        </MenuItem>
-        {/* <MenuItem component={RouterLink} to="/fixAccount">
-          <ListItemIcon>
-            <PortraitIcon />
-          </ListItemIcon>
-          個人情報修正
-        </MenuItem> */}
-        <MenuItem component={RouterLink} to="/">
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
-      </div>
-
-    </header>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleAccountClick}
+            style={{
+              fontSize: '25px', 
+              fontFamily: 'bold', 
+              padding: '0 30px 0 15px',
+              margin: '0 auto',
+              height: '60px',
+            }} 
+          >
+            <img src={menuIcon} alt="logo" style={{ width: '70px' }}/>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            style={{
+              marginTop: '45px',      
+            }} 
+          >
+            <MenuItem component={RouterLink} to="/myPage">
+              <ListItemIcon>
+                <FavoriteBorderIcon />
+              </ListItemIcon>
+              マイページ
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/myFolder">
+              <ListItemIcon>
+                <FolderSpecialIcon />
+              </ListItemIcon>
+              マイフォルダ
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/itemComparison">
+              <ListItemIcon>
+                <EqualizerIcon />
+              </ListItemIcon>
+              コスメ比較
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/unmatchedItem">
+              <ListItemIcon>
+                <NotInterestedIcon />
+              </ListItemIcon>
+              肌に合わなかったアイテム
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/requestPage">
+              <ListItemIcon>
+                <MailOutlineIcon />
+              </ListItemIcon>
+              リクエスト
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/">
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </div>
+      </header>
     </>
-    )
-}
+  );
+};
