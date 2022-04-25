0e33dfc46e5f01329ab3bfc1d3074600a2f846fb
shimizu <yoonairi0623@gmail.com>
2022-04-26 04:52:27 +0900

    左側img追加

--- frontend/src/pages/signUpPage.jsx
+++ frontend/src/pages/signUpPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
 import header_img from '../img/headerSignUp.jpg';
-
+import message_img from '../img/signIn_message.jpg';
 import { useNavigate } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -21,7 +21,6 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from "react-router-dom";
 import { Btn } from '../components/btn';
-
 import Tooltip from '@mui/material/Tooltip';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
@@ -31,14 +30,14 @@ import Select from '@mui/material/Select';
 
 const theme = createTheme();
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-    },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
 }));
 
 export const SignUpPage = () => {
@@ -50,23 +49,23 @@ export const SignUpPage = () => {
   const navigate = useNavigate();
 
   useEffect(async () => {
-    const responseGenders = await axios.get('/genders')
-    const responseSkinType = await axios.get('/skin_types')
-    const g = responseGenders.data
-    const s = responseSkinType.data
-    setGenders(g)
-    setSkinTypes(s)
-  }, [])
+    const responseGenders = await axios.get('/genders');
+    const responseSkinType = await axios.get('/skin_types');
+    const g = responseGenders.data;
+    const s = responseSkinType.data;
+    setGenders(g);
+    setSkinTypes(s);
+  }, []);
 
   const handleUserInformationChange = (event) => {
-    setUserInformation({...userInformation, [event.target.name]: event.target.value})
+    setUserInformation({...userInformation, [event.target.name]: event.target.value});
   };
 
   const handleRegister = async () => {
     try {
-      console.log(userInformation);
+      // console.log(userInformation);
       const response = await axios.post('/register', userInformation);
-      console.log(response);
+      // console.log(response);
       const loginResponse = await axios.post('/login', {
         email: userInformation.email,
         password: userInformation.password,
@@ -75,27 +74,27 @@ export const SignUpPage = () => {
       navigate("/homePage");
     } catch (e) {
       window.alert('登録に失敗しました');
-      console.error(e)
+      // console.error(e);
       return;
     }
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    // console.log({
+    //   email: data.get('email'),
+    //   password: data.get('password'),
+    // });
   };
 
-  const [values, setValues] = React.useState({
-        amount: '',
-        password: '',
-        weight: '',
-        weightRange: '',
-        showPassword: false,
+  const [values, setValues] = useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
   });
         
   const handleClickShowPassword = () => {
@@ -123,101 +122,91 @@ export const SignUpPage = () => {
 
   return (
     <div className='Main' style={{ margin: '60px auto 0' }}>
-    <ThemeProvider theme={theme}>
-    <Stack spacing={2} >
-            <Breadcrumbs
-            separator={<NavigateNextIcon fontSize="small" />}
-            aria-label="breadcrumb"
-            >
-            {breadcrumbs}
-            </Breadcrumbs>
-          </Stack>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+      <ThemeProvider theme={theme}>
+        <Stack spacing={2} >
+          <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
           >
-            <img src={header_img} alt="header" style={{width: '100%'}}/>
-
-
-            <Box component="form" noValidate onSubmit={handleSubmit} style={{ width: '80%' }}>
-              
-              <TextField
-                margin="normal"
-                style={{margin: '10px 0'}}
-                required
-                fullWidth
-                id="name"
-                label="ニックネーム"
-                name="name"
-                autoComplete="name"
-                autoFocus
-                onChange={handleUserInformationChange}
-              />
-
-            <form className={classes.container} noValidate>
-              <TextField
-                margin="normal"
-                style={{ margin: '2px 0' }}
-                required
-                fullWidth
-                id="date"
-                label="生年月日"
-                name="birth_date"
-                type="date"
-                className={classes.textField}
-                autoComplete="date"
-                autoFocus
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                onChange={handleUserInformationChange}
-              />
-            </form>          
-              <TextField
-                margin="normal"
-                style={{margin: '5px 0'}}
-                required
-                fullWidth
-                id="email"
-                label="メールアドレス"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={handleUserInformationChange}
-              />
-              <OutlinedInput
-                margin="normal"
-                style={{margin: '5px 0 10px'}}
-                required
-                fullWidth
-                name="password"
-                // TODO: ↓labelがきかない
-                label="パスワード"
-                type={values.showPassword ? 'text' : 'password'}
-                onChange={handleUserInformationChange}
-                id="password"
-                autoComplete="current-password"
-                endAdornment={
+            {breadcrumbs}
+          </Breadcrumbs>
+        </Stack>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            md={5}
+            sx={{
+              backgroundImage: `url(${message_img})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <Grid item xs={12} sm={8} md={7} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <img src={header_img} alt="header" style={{ width: '100%' }}/>
+              <Box component="form" noValidate onSubmit={handleSubmit} style={{ width: '80%' }}>                
+                <TextField
+                  margin="normal"
+                  style={{ margin: '10px 0' }}
+                  required
+                  fullWidth
+                  id="name"
+                  label="ニックネーム"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                  onChange={handleUserInformationChange}
+                />
+                <form className={classes.container} noValidate>
+                  <TextField
+                    margin="normal"
+                    style={{ margin: '2px 0' }}
+                    required
+                    fullWidth
+                    id="date"
+                    label="生年月日"
+                    name="birth_date"
+                    type="date"
+                    className={classes.textField}
+                    autoComplete="date"
+                    autoFocus
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={handleUserInformationChange}
+                  />
+                </form>          
+                <TextField
+                  margin="normal"
+                  style={{ margin: '5px 0' }}
+                  required
+                  fullWidth
+                  id="email"
+                  label="メールアドレス"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={handleUserInformationChange}
+                />
+                <OutlinedInput
+                  margin="normal"
+                  style={{ margin: '5px 0 10px' }}
+                  required
+                  fullWidth
+                  name="password"
+                  // TODO: ↓labelがきかない
+                  label="パスワード"
+                  type={values.showPassword ? 'text' : 'password'}
+                  onChange={handleUserInformationChange}
+                  id="password"
+                  autoComplete="current-password"
+                  endAdornment={
                     <InputAdornment position="end">
                       <IconButton
                         aria-label="toggle password visibility"
@@ -229,74 +218,70 @@ export const SignUpPage = () => {
                       </IconButton>
                     </InputAdornment>
                   }
-              />
-
-              <FormControl style={{margin: '20px 20px 0 0'}}>
-                <InputLabel id="demo-simple-select-helper-label">性別</InputLabel>
-                <Select
-                name="gender_id"
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={userInformation.gender_id}
-                label="gender_id"
-                onChange={handleUserInformationChange}
-                style={{
-                  margin: '0 auto',
-                  minWidth: '200px',
-                }}
-                >
-                  {genders.map((gender) => (
-                    <MenuItem value={gender.id}>{gender.name}</MenuItem>
-                  ))}                                  
-                </Select>
-                <p style={{ color: '#f34040ac', fontSize: '10px' }}>性別は登録すると変更できません。</p>
-
-              </FormControl>
-
-              <FormControl style={{marginTop: '20px'}}>
-                <InputLabel id="demo-simple-select-helper-label">スキンタイプ</InputLabel>
-                <Tooltip title={skinTypeTooltip} followCursor>
+                />
+                <FormControl style={{ margin: '20px 20px 0 0' }}>
+                  <InputLabel id="demo-simple-select-helper-label">性別</InputLabel>
                   <Select
-                    name="skin_type_id"
-                    labelId="demo-simple-select-helper-label"
-                    id="demo-simple-select-helper"
-                    value={userInformation.skin_type_id}
-                    label="skin_type_id"
-                    onChange={handleUserInformationChange}
-                    // TODO: ↓？
-                    onClose={() => { setSkinTypeTooltip(''); }}
-                    style={{
-                      margin: '0 auto',
-                      minWidth: '200px',
-                    }}
+                  name="gender_id"
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={userInformation.gender_id}
+                  label="gender_id"
+                  onChange={handleUserInformationChange}
+                  style={{
+                    margin: '0 auto',
+                    minWidth: '200px',
+                  }}
                   >
-                    {skinTypes.map((skinType) => (
-                      <MenuItem
-                        value={skinType.id} 
-                        style={{ margin: '5px' }}
-                        // TODO: ↓？
-                        onMouseOver={() => { setSkinTypeTooltip(skinType.detail); }}
-                      >
-                      {skinType.name}
-                      </MenuItem>
-                    ))}
+                    {genders.map((gender) => (
+                      <MenuItem value={gender.id}>{gender.name}</MenuItem>
+                    ))}                                  
                   </Select>
-                </Tooltip>
-              </FormControl>          
+                  <p style={{ color: '#f34040ac', fontSize: '10px' }}>性別は登録すると変更できません。</p>
+                </FormControl>
+                <FormControl style={{ marginTop: '20px' }}>
+                  <InputLabel id="demo-simple-select-helper-label">スキンタイプ</InputLabel>
+                  <Tooltip title={skinTypeTooltip} followCursor>
+                    <Select
+                      name="skin_type_id"
+                      labelId="demo-simple-select-helper-label"
+                      id="demo-simple-select-helper"
+                      value={userInformation.skin_type_id}
+                      label="skin_type_id"
+                      onChange={handleUserInformationChange}
+                      // TODO: ↓？
+                      onClose={() => { setSkinTypeTooltip(''); }}
+                      style={{
+                        margin: '0 auto',
+                        minWidth: '200px',
+                      }}
+                    >
+                      {skinTypes.map((skinType) => (
+                        <MenuItem
+                          value={skinType.id} 
+                          style={{ margin: '5px' }}
+                          // TODO: ↓？
+                          onMouseOver={() => { setSkinTypeTooltip(skinType.detail); }}
+                        >
+                        {skinType.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </Tooltip>
+                </FormControl>          
+                <div style={{ margin: '20px' }}>
+                  <p style={{ color: 'gray' }}>登録情報はマイページからいつでも変更できます。</p>
+                </div>
 
-              <div style={{ margin: '20px' }}>
-                <p style={{ color: 'gray' }}>登録情報はマイページからいつでも変更できます。</p>
-              </div>
-
-            <Btn
-              onClick={handleRegister}
-              message='登録する'
-            />
+                <Btn
+                  onClick={handleRegister}
+                  message='登録する'
+                />
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
     </div>
   );
 };
