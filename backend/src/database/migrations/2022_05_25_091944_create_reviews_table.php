11b8e5850a6f58ada4726fe3a245681e3590ccac
shimizu <yoonairi0623@gmail.com>
2022-05-25 22:09:39 +0900

    reviewsテーブル作成

---
+++ backend/src/database/migrations/2022_05_25_091944_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->text('review');
+            $table->integer('star');
+            $table->date('posted_date');
+            $table->foreignId('item_id')->constrained('items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
