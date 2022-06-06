729b8fc7b94cdf5fadb9569dbacd8498af4a6d5a
shimizu <yoonairi0623@gmail.com>
2022-06-07 01:50:24 +0900

    updated_at追加

--- backend/src/app/Models/UserHistory.php
+++ backend/src/app/Models/UserHistory.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class UserHistory extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['item_id', 'user_id'];
+    protected $fillable = ['item_id', 'user_id', 'updated_at'];
+
 }
