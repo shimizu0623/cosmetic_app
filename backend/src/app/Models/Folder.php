65cdc8b16bf610dfc766276e066a29b954e9d4f4
shimizu <yoonairi0623@gmail.com>
2022-06-15 21:02:19 +0900

    toArrayへingredientScores作成

--- backend/src/app/Models/Folder.php
+++ backend/src/app/Models/Folder.php
@@ -16,13 +16,38 @@ class Folder extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function toArray() 
+    public function items()
     {
+        return $this->belongsToMany(Item::class, FolderItem::class);
+    }
+
+    public function toArray()
+    {
+        $ingredientScores = $this->items->map(
+            function($item) {
+                return $item->ingredients->map(function($ingredient) { return $ingredient->score; });
+            })->flatten();
+
         return [
           'id' => $this->id,
           'user_id' => $this->user->id,
           'name' => $this->name,
+          'items' => $this->items->map(function($item) { return $item->toArray(); }),
+          'green' => $ingredientScores
+            ->filter(function($score) { return 1 <= $score && $score <= 2; })
+            ->count(),
+          'yellow' => $ingredientScores
+            ->filter(function($score) { return 3 <= $score && $score <= 6; })
+            ->count(),
+          'red' => $ingredientScores
+            ->filter(function($score) { return 7 <= $score && $score <= 10; })
+            ->count(),
+          'gray' => $ingredientScores
+            ->filter(function($score) { return -1 === $score; })
+            ->count(),
+
+        //   'item_id' => $this->items->map(function($item) { return $item->id; }),
+        //   'item_img' => $this->items->map(function($item) { return $item->img; }),
         ];
     }
-    
 }
